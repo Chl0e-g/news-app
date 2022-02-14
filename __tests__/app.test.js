1622ee857c56121550c7bc8dc0be857aca64db04
Chloe <chloe_g@btinternet.com>
2022-02-14 11:53:38 +0000

    GET /api/topics endpoint created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,22 @@
 const request = require("supertest");
 const app = require("../app");
 const seed = require("../db/seeds/seed");
-const data = require("../db/data");
-const db = require("../db");
+const data = require("../db/data/test-data");
+const db = require("../db/connection");
 
 afterAll(() => db.end());
 beforeEach(() => seed(data));
 
+describe("/api/topics", () => {
+  describe("GET", () => {
+    test("status: 200 - responds with an array of topic objects", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body: topics }) => {
+          expect(Array.isArray(topics)).toBe(true);
+          expect(topics).toHaveLength(3);
+        });
+    });
+  });
+});
