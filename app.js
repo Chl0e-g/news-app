45d275236cea0f7682206a6da993d26b169a4e23
Chloe <chloe_g@btinternet.com>
2022-02-14 12:16:36 +0000

    added 500 internal server error handling

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
+const { invalidPath } = require("./error-handlers/app.error-handlers");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.all("/*", invalidPath);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal server error" });
+});
 
 module.exports = app;
