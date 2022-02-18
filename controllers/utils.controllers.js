7bbccd2f32cc825a9012784ade7dd7c94f63f1fe
Chloe <chloe_g@btinternet.com>
2022-02-18 11:32:31 +0000

    add catch block to controller

--- controllers/utils.controllers.js
+++ controllers/utils.controllers.js
@@ -1,7 +1,11 @@
 const { fetchEndpoints } = require("../models/utils.models");
 
-exports.getEndpoints = (req, res) => {
-  fetchEndpoints().then((endpoints) => {
-    res.status(200).send({ endpoints });
-  });
+exports.getEndpoints = (req, res, next) => {
+  fetchEndpoints()
+    .then((endpoints) => {
+      res.status(200).send({ endpoints });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
